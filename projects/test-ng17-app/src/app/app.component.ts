fc5f37a1cf8dbc49614a466c8ed8408b89bad644
Stuart Rimel <stuart_rimel@trimble.com>
2024-11-14 13:40:27 -0800

    test consume modus 2.0 components

--- projects/test-ng17-app/src/app/app.component.ts
+++ projects/test-ng17-app/src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'test-ng17-app';
+  avatarImgSrc =
+    'https://i.pinimg.com/474x/73/54/79/7354794bf3873c3ef2666f778da4bcac.jpg';
 }
--- projects/test-ng17-app/src/app/app.module.ts
+++ projects/test-ng17-app/src/app/app.module.ts
@@ -4,15 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ModusAngularComponentsModule } from '@trimble-cms/modus-wc-angular';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ModusAngularComponentsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
