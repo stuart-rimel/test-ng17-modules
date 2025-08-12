dbf8756e253d887ea27558c101144f2b1068283d
Stuart Rimel <stuart_rimel@trimble.com>
2025-08-12 12:14:19 -0700

    add modal example

--- projects/test-ng17-app/src/app/app.component.ts
+++ projects/test-ng17-app/src/app/app.component.ts
@@ -3,10 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'test-ng17-app';
   avatarImgSrc =
     'https://i.pinimg.com/474x/73/54/79/7354794bf3873c3ef2666f778da4bcac.jpg';
+
+  openModal(): void {
+    (
+      document.getElementById('my_modal_1ulsp') as HTMLDialogElement
+    )?.showModal();
+  }
+
+  closeModal(): void {
+    (document.getElementById('my_modal_1ulsp') as HTMLDialogElement)?.close();
+  }
 }
--- projects/test-ng17-app/src/app/app.module.ts
+++ projects/test-ng17-app/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { ModusAngularComponentsModule } from '@trimble-cms/modus-wc-angular';
+import { ModusAngularComponentsModule } from '@trimble-oss/moduswebcomponents-angular';
 import { TestFormComponent } from './test-form/test-form.component';
 import { TestFormInputComponent } from './test-form-input/test-form-input.component';
 import { TestFormTextareaComponent } from './test-form-textarea/test-form-textarea.component';
